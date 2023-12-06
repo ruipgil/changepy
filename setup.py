bf53e3a4af182b38b9c08fc56a0c6e68c2d33bda
Rui Gil <ruipgil@gmail.com>
2023-12-06 20:36:03 +0000

    bump to v0.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='changepy',
-    version='0.3.1',
+    version='0.4.0',
     description='Changepoint detection on time series, in Python',
     author='Rui Gil',
     author_email='ruipgil@gmail.com',
